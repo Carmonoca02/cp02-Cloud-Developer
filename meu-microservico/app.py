e429b4b623d7782cc49027edde44edb548c04cf4
Carmonoca02 <rm557064@fiap.com.br>
2025-04-12 20:11:27 -0300

    CP2-Exercicio6

---
+++ meu-microservico/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/status', methods=['GET'])
+def status():
+    return jsonify({
+        "status": "online",
+        "message": "O microsservico esta funcionando!"
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
